1e25d87b98b2c0d06511143176c523efb4ba7f56
Mike Xia <mxia@tugo.com>
2021-01-27 22:56:04 -0800

    commit

--- oauth/src/main/java/com/example/dms/auth/config/ExceptionConfig.java
+++ oauth/src/main/java/com/example/dms/auth/config/ExceptionConfig.java
@@ -1,6 +1,8 @@
 package com.example.dms.auth.config;
 
 import brave.Tracer;
+import com.example.dms.auth.exception.BaseException;
+import com.example.dms.auth.ro.ErrorRO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,8 +18,12 @@ public class ExceptionConfig {
     Tracer tracer;
 
     @ExceptionHandler(Exception.class)
-    private ResponseEntity exceptionHandler(Exception exception) {
-        exception.printStackTrace();
-        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    private ResponseEntity<ErrorRO> exceptionHandler(BaseException exception) {
+        log.warn(exception.getMessage());
+        String traceInfo = tracer.currentSpan().context().traceIdString()
+                .concat("|")
+                .concat(tracer.currentSpan().context().spanIdString());
+        ErrorRO errorRO = new ErrorRO(exception.getError(), exception.getMessage(), traceInfo);
+        return new ResponseEntity<>(errorRO, exception.getStatus());
     }
 }
--- oauth/src/main/java/com/example/dms/auth/controller/AuthController.java
+++ oauth/src/main/java/com/example/dms/auth/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.example.dms.auth.controller;
 
 import com.example.dms.auth.bridge.TokenBridge;
 import com.example.dms.auth.entity.UserEntity;
+import com.example.dms.auth.exception.InvalidPasswordException;
+import com.example.dms.auth.exception.InvalidUserNameException;
 import com.example.dms.auth.model.Token;
 import com.example.dms.auth.ro.LoginRO;
 import com.example.dms.auth.ro.TokenRO;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,10 +39,10 @@ public class AuthController {
                 TokenRO tokenRO = tokenBridge.convertTokenToTokenRO(token);
                 return new ResponseEntity<>(tokenRO, HttpStatus.OK);
             } else {
-                return new ResponseEntity("User login information not correct", HttpStatus.BAD_REQUEST);
+                throw new InvalidPasswordException();
             }
         } else {
-            return new ResponseEntity("User login inforation not correct", HttpStatus.BAD_REQUEST);
+            throw new InvalidUserNameException();
         }
     }
 }
